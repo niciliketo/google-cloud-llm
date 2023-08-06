8bd7a10ea1295fa39921b1df0490e0dc9dd6b457
Nicholas Martin <nicholas.martin@marketdojo.com>
2023-08-06 12:48:09 +0100

    Dockerise
    Dockerise, setup vscode config

--- spec/gcp_llm_spec.rb
+++ spec/gcp_llm_spec.rb
@@ -5,34 +5,34 @@ RSpec.describe GcpLlm do
 
   describe "#configure" do
     let(:access_token) { "abc123" }
-    let(:api_version) { "v2" }
-    let(:organization_id) { "def456" }
-    let(:custom_uri_base) { "ghi789" }
+    let(:project_id) { "v2" }
+    let(:api_endpoint) { "def456" }
+    let(:model_id) { "ghi789" }
+    let(:custom_uri_base) { "https://us-central1-aiplatform.googleapis.com" }
     let(:custom_request_timeout) { 25 }
 
     before do
       GcpLlm.configure do |config|
         config.access_token = access_token
-        config.api_version = api_version
-        config.organization_id = organization_id
+        config.project_id = project_id
+        # config.organization_id = organization_id
       end
     end
 
     it "returns the config" do
       expect(GcpLlm.configuration.access_token).to eq(access_token)
-      expect(GcpLlm.configuration.api_version).to eq(api_version)
-      expect(GcpLlm.configuration.organization_id).to eq(organization_id)
-      expect(GcpLlm.configuration.uri_base).to eq("https://api.gcp_llm.com/")
+      expect(GcpLlm.configuration.project_id).to eq(project_id)
+      expect(GcpLlm.configuration.uri_base).to eq("https://us-central1-aiplatform.googleapis.com/v1/projects/")
       expect(GcpLlm.configuration.request_timeout).to eq(120)
     end
 
-    context "without an access token" do
-      let(:access_token) { nil }
+    # context "without an access token" do
+    #   let(:access_token) { nil }
 
-      it "raises an error" do
-        expect { GcpLlm::Client.new.completions }.to raise_error(GcpLlm::ConfigurationError)
-      end
-    end
+    #   it "raises an error" do
+    #     expect { GcpLlm::Client.new.completions }.to raise_error(GcpLlm::ConfigurationError)
+    #   end
+    # end
 
     context "with custom timeout and uri base" do
       before do
@@ -44,8 +44,8 @@ RSpec.describe GcpLlm do
 
       it "returns the config" do
         expect(GcpLlm.configuration.access_token).to eq(access_token)
-        expect(GcpLlm.configuration.api_version).to eq(api_version)
-        expect(GcpLlm.configuration.organization_id).to eq(organization_id)
+        # expect(GcpLlm.configuration.api_version).to eq(api_version)
+        expect(GcpLlm.configuration.project_id).to eq(project_id)
         expect(GcpLlm.configuration.uri_base).to eq(custom_uri_base)
         expect(GcpLlm.configuration.request_timeout).to eq(custom_request_timeout)
       end
